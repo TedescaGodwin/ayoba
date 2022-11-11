b8b048334ab128cc009d214418e181d3b2f30e6b
Tedesca <tedesca7@outlook.com>
2022-11-11 02:25:42 +0200

    JWT and Swagger Integration

--- config/default.ts
+++ config/default.ts
@@ -2,5 +2,10 @@ export default {
     port: 1337,
     dbUri: "mongodb://0.0.0.0:27017/ayoba-music", 
     saltWorkFactor: 10,
-    
+    accessTokenTtl:"15m",
+    refreshTokenTtl:"1y",
+    accessTokenPrivateKey: ``,
+    accessTokenPublicKey: ``,
+    refreshTokenPrivateKey: ``,
+    refreshTokenPublicKey: ``,
   }
--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import config from "config";
 import logger from "./utils/logger";
 import connect from './utils/connect';
 import routes from "./routes";
+import deserializeUser from "./middleware/deserializeUser";
 
 const port = config.get<number>("port");
 
@@ -10,7 +11,7 @@ const app = express();
 
 // Parses incoming requests with JSON payloads
 app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
+app.use(deserializeUser);
 
 app.listen(port, async () => {
   logger.info(`App running at http://localhost:${port}`);
